c26040831e0bd2ad560b80a4a1ca8d2a0b5d6963
nsnrfuture <nsnrfuturesoftware@gmail.com>
2025-11-22 04:00:05 +0530

    contact page and term page 2

--- src/components/common/TegoFooter.tsx
+++ src/components/common/TegoFooter.tsx
@@ -1,14 +1,14 @@
 export default function TegoFooter() {
   return (
     <footer className="bg-[#111] border-t border-white/10 mt-10 py-6 px-6 md:px-10 text-center text-white/60 text-sm">
-      <p>© {new Date().getFullYear()} Tego — All rights reserved.</p>
+      <p>© {new Date().getFullYear()} Tego Live— All rights reserved.</p>
 
       <div className="mt-3 flex justify-center gap-6 text-xs">
         <a href="/privacy-policy
         " className="hover:text-white transition">Privacy Policy</a>
         <a href="/terms-and-conditions" className="hover:text-white transition">Terms of Conditions</a>
-        <a href="/contact" className="hover:text-white transition">Contact</a>
         <a href="/cancellations-and-refunds" className="hover:text-white transition">Cancellations and Refunds</a>
+        <a href="/contact" className="hover:text-white transition">Contact</a>
       </div>
     </footer>
   );
